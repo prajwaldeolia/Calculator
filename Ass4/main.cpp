f89f9a619370ea02ffd9405f182214ced9a1fdcb
Prajwal Deolia <54313411+prajwaldeolia@users.noreply.github.com>
2023-01-25 16:50:42 +0530

    Update main.cpp

--- Ass4/main.cpp
+++ Ass4/main.cpp
@@ -5,46 +5,46 @@
 #include<gtest/gtest.h>
 #define pi 3.14
 
-// int main(int argc, char **argv)
-// {
-//     testing::InitGoogleTest(&argc, argv);
-//     std::string query;
-//     std::cout << "simple calc only use : + , - , * , /, () " << std::endl;
-//     while(true)
-//     {
-//         std::cout << "Enter Query : ";
-//         std::cin >> query;
-//         CALCULATOR::Get().calulate(query);
-//         lable:
-//         std::cout<<"for continue press 1\nfor exit press 0"<<std::endl;
-//         int response;
-//         std::cin>>response;
-//         if(response == 1)
-//             continue;
-//         else if(response == 0)
-//             break;
-//         else
-//         {
-//             std::cout<<"enter correct choice\n";
-//             goto lable;
-//         }
-//     }
-//     std::cout<<"check History -> press 1\nto exit hit 'enter'";
-//     int a = 0;
-//     std::cin>>a;
-//     if(a == 1)
-//         CALCULATOR::Get().showHistory();
-//     std::cin.get();
-//     return RUN_ALL_TESTS();
-// }
-
-
-
-
-
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
-    
+    std::string query;
+    std::cout << "simple calc only use : + , - , * , /, () " << std::endl;
+    while(true)
+    {
+        std::cout << "Enter Query : ";
+        std::cin >> query;
+        CALCULATOR::Get().calulate(query);
+        lable:
+        std::cout<<"for continue press 1\nfor exit press 0"<<std::endl;
+        int response;
+        std::cin>>response;
+        if(response == 1)
+            continue;
+        else if(response == 0)
+            break;
+        else
+        {
+            std::cout<<"enter correct choice\n";
+            goto lable;
+        }
+    }
+    std::cout<<"check History -> press 1\nto exit hit 'enter'";
+    int a = 0;
+    std::cin>>a;
+    if(a == 1)
+        CALCULATOR::Get().showHistory();
+    std::cin.get();
     return RUN_ALL_TESTS();
 }
+
+
+
+
+
+// int main(int argc, char **argv)
+// {
+//     testing::InitGoogleTest(&argc, argv);
+    
+//     return RUN_ALL_TESTS();
+// }
